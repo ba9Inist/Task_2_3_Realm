1057c0bdd05a3fe5c59b5b981a6df8219e01352f
Golubev Egor <ged@mosremit.ru>
2025-05-29 15:12:17 +0300

    Initial Commit

---
+++ Task_2_3_Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_2_3_Realm
+//
+//  Created by Егор Голубев on 29.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
